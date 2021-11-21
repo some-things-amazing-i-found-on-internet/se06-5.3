48710a7dd45f10923ab7ee10230544e4cb9ffba2
= <=>
2021-11-21 10:02:11 +0700

    insert data orders from users into database

--- src/views/pages/restaurant.php
+++ src/views/pages/restaurant.php
@@ -300,7 +300,7 @@
 
 
                             </div>
-                            <div class="card-footer padding-15" id="proceed-checkout"> <a href="checkout.html" class="btn-first green-btn text-custom-white full-width fw-500">Proceed to Checkout</a>
+                            <div class="card-footer padding-15" id="proceed-checkout"> <a href="checkout" class="btn-first green-btn text-custom-white full-width fw-500">Proceed to Checkout</a>
                             </div>
                         </div>
                     </div>
