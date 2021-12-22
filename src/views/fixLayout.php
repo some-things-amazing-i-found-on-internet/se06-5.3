f158e20a74d53ad6c74238e3060530bd4bebb621
Nguyễn Đức Huy <nguyenduchuy_t64@hus.edu.vn>
2021-12-22 10:33:39 +0700

    complete filter

--- src/views/fixLayout.php
+++ src/views/fixLayout.php
@@ -40,6 +40,7 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&amp;display=swap" rel="stylesheet">
     <!-- place -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
