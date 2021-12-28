7eaa47bbf0183d8cbdf2c4439b2b9bc2ebec9878
= <=>
2021-12-28 19:43:47 +0700

    update database

--- database/pre_orders.sql
+++ database/pre_orders.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2021 at 12:20 PM
+-- Generation Time: Dec 28, 2021 at 01:41 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -31,47 +31,11 @@ CREATE TABLE `pre_orders` (
   `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
-  `food_id` int(11) NOT NULL,
+  `food_id` varchar(12) NOT NULL,
   `quantity_order` int(11) NOT NULL,
   `delivery_fee` int(11) NOT NULL,
   `order_status` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `pre_orders`
---
-
-INSERT INTO `pre_orders` (`id`, `order_id`, `customer_id`, `food_id`, `quantity_order`, `delivery_fee`, `order_status`) VALUES
-(94, 0, 8, 10751832, 5, 27000, 0),
-(95, 0, 8, 10751831, 1, 27000, 0),
-(96, 0, 8, 10751834, 1, 27000, 0),
-(97, 3, 8, 10679993, 1, 13000, 0),
-(98, 3, 8, 10679999, 2, 13000, 0),
-(99, 5, 8, 11120157, 4, 17000, 0),
-(100, 5, 8, 11120160, 4, 17000, 0),
-(101, 7, 8, 12669712, 1, 10000, 0),
-(102, 7, 8, 12669729, 2, 10000, 0),
-(103, 9, 8, 2322066, 1, 28000, 0),
-(104, 9, 8, 2322078, 3, 28000, 0),
-(105, 11, 8, 11325505, 1, 27000, 0),
-(106, 11, 8, 11325516, 4, 27000, 0),
-(107, 13, 8, 7822254, 1, 29000, 0),
-(108, 13, 8, 7822276, 1, 29000, 0),
-(109, 13, 8, 7822285, 1, 29000, 0),
-(110, 13, 8, 10762734, 2, 29000, 0),
-(111, 17, 8, 2322078, 1, 11000, 0),
-(112, 17, 8, 2322080, 2, 11000, 0),
-(113, 17, 8, 2322066, 1, 11000, 0),
-(114, 17, 8, 2322212, 1, 11000, 0),
-(115, 17, 8, 2322285, 2, 11000, 0),
-(116, 22, 8, 10762734, 1, 26000, 0),
-(117, 22, 8, 10762735, 1, 26000, 0),
-(118, 22, 8, 7822230, 1, 26000, 0),
-(119, 22, 8, 7822254, 1, 26000, 0),
-(120, 26, 8, 10679999, 1, 12000, 0),
-(121, 26, 8, 10679993, 2, 12000, 0),
-(122, 28, 8, 11325505, 2, 10000, 0),
-(123, 28, 8, 11325516, 4, 10000, 0);
 
 --
 -- Indexes for dumped tables
@@ -91,7 +55,7 @@ ALTER TABLE `pre_orders`
 -- AUTO_INCREMENT for table `pre_orders`
 --
 ALTER TABLE `pre_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=124;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=234;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
